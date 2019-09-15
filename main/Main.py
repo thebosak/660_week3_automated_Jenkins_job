35f4c90cbc5ebb9e8fb58f8c6355a98e0b16c378
thebosak <33043855+thebosak@users.noreply.github.com>
2019-09-14 21:24:28 -0500

    initial commit for main function

---
+++ main/Main.py
@@ -0,0 +1,14 @@
+'''
+Created on September 14, 2019
+
+@author: Brad Bosak
+'''
+
+#Create main function
+def main():
+    x = int(input('Enter the first number to multiply: '))
+    y = int(input('Enter the second number to multiply: '))
+    print('The product is ', x*y)
+    
+#Call main function
+main()
